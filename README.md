358a0882db49f139556bc711c5e876d6b029990f
Todd Bryant <bryantt@dickinson.edu>
2019-04-29 15:18:36 -0400

    Removed unnecessary files and commented code

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # friend flag
 Friend Module for Drupal
-Very simple "friend" module built upon Flag module. Then install this module. Need to create a user flag called friend. It will create block for friend activity. The block is table with two columns: friend requests (with a link to add the user as a friend), and a friends column. Principle is a user clicks the flag on another user, that is a friend request. If two users each click the friend flag on the other's profile page, they are friends.
+Very simple "friend" module built upon Flag module. Then install this module. Need to create a user flag called friend. The machine name of the link must be 'friend' to work.  Choose the AJAX link for the link type, you'll also likely want to check the box 'Permissions for users to flag themselves.' and then deny the permission.  Note, you need to be sure not to grant permission for general 'flag friend' permission as well as the 'flag friend own profile'.  They should only have 'Flag friend other's profile' and 'Unflag friend other's profile'
+
+This module will then  create a block for friend activity. The block is table with two columns: friend requests (with a link to add the user as a friend), and a friends column. Principle is a user clicks the flag onanother user, that is a friend request. If two users each click the friend flag on the other's profile page, they are friends.
 
 Screenshots available at https://www.drupal.org/sandbox/bryantt/2987656
 
 For most users, you'll only want their "friend" block visible to them and on a certain page. If this is the case, add the block via Block Layout and restrict access via the Block Layout under structure.
 
-Note this assumes your url for each account is yourdomainname.com/user/theirUserID. If you have additional "/"s, add one to the $arg[] for each additional / in the url before the user id. 
+
+ 
--- src/Plugin/Block/FriendFlagBlock.php
+++ src/Plugin/Block/FriendFlagBlock.php
@@ -5,8 +5,7 @@ namespace Drupal\friend_flag\Plugin\Block;
 use Drupal\user\Entity\User;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Url;
-
-
+use Drupal\Component\Utility\Xss;
 
 /**
  * @Block(
@@ -23,14 +22,9 @@ class FriendFlagBlock extends Blockbase {
 
     return array(
       '#markup' => $this->_populate_markup(),
-//	  '#friendRequestsReceived' => $this->_friend_requests_received(),
-//	  '#friendRequestsSent' => $this->_friend_requests_sent(),
-//	  '#friendList' => $this->_friend_list(),
-//    '#link' => 'http://www.example.com/',
       '#attached' => array(
         'library' => array(
           'friend_flag/friend_flag',
-		  'friend_flag/growDiv',
         ),
       ),
     );
@@ -42,99 +36,14 @@ class FriendFlagBlock extends Blockbase {
   public function getCacheMaxAge() {
       return 0;
   }
-/**
-  private function _friend_requests_received()  {
-    $friendRequestsReceived = Markup::create($friendRequestsReceived);
-	$friendRequestsReceived = 'Friend Requests Received</br><ol>';
 
-    $user = User::load(\Drupal::currentUser()->id());
-	$currentUserID = $user->get('uid')->value;
-	$home_url = Url::fromRoute('<front>', [], ['absolute' => TRUE])->ToString();
-	$database = \Drupal::database();
-
-	$resultReceived = $database->select('flagging', 'f')
-	  ->fields('f')
-	  ->condition('entity_id', $currentUserID)
-	  ->condition('flag_id', 'friend')
-	  ->orderby('created', 'DESC')
-	  ->execute();
-	//If no results, leave a message for the column
-	$resultReceived->allowRowCount = TRUE;
-    		// Now we loop through results and return a link to the user who sent the request, a link to friend, and 'new' if within past two days.
-			// The counter is to keep track of the requests received who aren't friends.  If counter is 0, leave a message
-			$Count = 0;
-			foreach ($resultReceived as $recordReceived)  {
-	  		//We further limit to remove people who are already friends
-	  		//query uid = strCurrentUser and entity_id = recordReceived->uid.  If Not TRUE, do $friendString section
-				$Count += 1;
-	  			$alreadyFriendsCount = $database->query("SELECT * FROM {flagging} WHERE uid = :uid and entity_id = :entity_id", [':uid' => $currentUserID, 'entity_id' => $recordReceived->uid]);
-	  			$alreadyFriendsCount->allowRowCount = TRUE;
-	  			if($alreadyFriendsCount->rowCount() == 0){
-	  				$Friend_ID = $recordReceived->uid;
-	  				$Created_Timestamp = $recordReceived->created;
-	  				$Friend_Name = $database->query("SELECT name FROM {users_field_data} WHERE uid = :uid", array(':uid' => $Friend_ID))->fetchField();
-
-
-
-	  				//if less that two days print "new" afterwards in red.
-					$intTimeAgoFriendReceived = time() - $Created_Timestamp;
-					if ($intTimeAgoFriendReceived < 172800){
-						$strNew = " - <b>New</b>";
-					}else{
-						$strNew ="";
-					}
-
-					//generate url to create a friend link
-					$url = Url::fromRoute('flag.action_link_flag',['flag' => 'friend', 'entity_id' => $Friend_ID]);
-					$token = \Drupal::csrfToken()->get($url->getInternalPath());
-					$url->setOptions(['absolute' => TRUE, 'query' => ['token' => $token]]);
-					$friend_url = $url->toString();
-
-	    			$friendRequestsReceived .= "<li><a href='" . $home_url . "user/" . $Friend_ID ."'>" . $Friend_Name . "</a> | <div class='flag flag-friend js-flag-friend-" . $Friend_ID ." action-flag'><a title='' href='" . $friend_url . "' class='use-ajax' rel='nofollow'>Add as friend</a></div>" . $strNew . "</li>";
-				}else{
-					$Count -= 1;
-				}
-
-	  }
-	$friendRequestsReceived .= '</ol>';
-
-	if ($Count == 0){
-		$friendRequestsReceived = "No pending friend requests";
-	}
-	
-
-
-	return $friendRequestsReceived;
-  }
-
-  private function _friend_requests_sent()  {
-	$friendRequestsSent = 'Friend Requests Sent</br>';
-
-    $user = User::load(\Drupal::currentUser()->id());
-	$currentUserID = $user->get('uid')->value;
-	$home_url = Url::fromRoute('<front>', [], ['absolute' => TRUE])->ToString();
-	$database = \Drupal::database();
-
-
-	return $friendRequestsSent;
-  }
-
-  private function _friend_list()  {
-	$friendList = 'My friends</br>';
-
-    $user = User::load(\Drupal::currentUser()->id());
-	$currentUserID = $user->get('uid')->value;
-	$home_url = Url::fromRoute('<front>', [], ['absolute' => TRUE])->ToString();
-	$database = \Drupal::database();
-
-
-
-	return $friendList;
-
-  }
-*/
+  /**
+  * We'll query the database to create a html string for a table with two columns.
+  * Friend Requests Received column means the user was the target of the 'friend'
+  * flag.  
+  */ 
   private function _populate_markup() {
-    $user = User::load(\Drupal::currentUser()->id());
+  $user = User::load(\Drupal::currentUser()->id());
 	$currentUserID = $user->get('uid')->value;
 	$home_url = Url::fromRoute('<front>', [], ['absolute' => TRUE])->ToString();
 
@@ -142,9 +51,8 @@ class FriendFlagBlock extends Blockbase {
 	$friendString = "<table class='friendTable'><tr>";
 	$database = \Drupal::database();
 
-  /*
-   * Begin 'Friend Requests Received' Column.  Requests received are the rows in the flagging table where entity_id is the current user
-  */
+
+  // Begin 'Friend Requests Received' Column.  Requests received are the rows in the flagging table where entity_id is the current user
   $friendString .= "<td valign='top' class='friendRequests'><b>Friend Requests Received</b></br><ul>";
 	$resultReceived = $database->select('flagging', 'f')
 	  ->fields('f')
@@ -170,12 +78,12 @@ class FriendFlagBlock extends Blockbase {
 
 
 
-	  				//if less that two days print "new" afterwards in red.
+	  			//if less that two days print "new" afterwards in red.
 					$intTimeAgoFriendReceived = time() - $Created_Timestamp;
 					if ($intTimeAgoFriendReceived < 172800){
-						$strNew = " <b>(New)</b>&nbsp;";
+						$strNew = "&nbsp;<b>(New)</b>&nbsp;";
 					}else{
-						$strNew ="";
+						$strNew = "";
 					}
 
 					//generate url to create a friend link
@@ -184,11 +92,12 @@ class FriendFlagBlock extends Blockbase {
 					$url->setOptions(['absolute' => TRUE, 'query' => ['token' => $token]]);
 					$friend_url = $url->toString();
 
-	    			$friendString .= "<li><a href='" . $home_url . "user/" . $Friend_ID ."'>" . $Friend_Name . "</a><div class='flag flag-friend js-flag-friend-" . $Friend_ID ." action-flag'><a title='' href='" . $friend_url . "' class='use-ajax' rel='nofollow'>Add as friend</a></div> " . $strNew . "</li>";
+	    			$friendString .= "<li><div class='flag-friend-name'><a href='" . $home_url . "user/" . $Friend_ID ."'>" . $Friend_Name . "</a></div> " . $strNew . 
+              "<div class='flag flag-friend js-flag-friend-" . $Friend_ID ." action-flag'><a title='' href='" . $friend_url . 
+              "' class='use-ajax' rel='nofollow'>Add as friend</a></div></li>";
 				}else{
 					$Count -= 1;
 				}
-
 	  }
 
 	if ($Count == 0){
@@ -197,35 +106,9 @@ class FriendFlagBlock extends Blockbase {
     
 	$friendString .= "</td>";
 
-  /*
-   * Begin 'Friend Requests Sent' Column.  Requests sent are the rows in the flagging table where uid is the current user
-  */
-  //Decided I don't want this column
-  /*
-  $friendString .= "<td valign='top' class='friendRequestsSent'><b>Friend Requests Sent</b></br><ul>";
-	$resultSent = $database->select('flagging', 'f')
-	  ->fields('f')
-	  ->condition('uid', $currentUserID)
-	  ->condition('flag_id', 'friend')
-	  ->orderby('created', 'DESC')
-	  ->execute();
-
-	foreach ($resultSent as $recordSent)  {
-	  $Friend_ID = $recordSent->entity_id;
-	  $Created_Timestamp = $recordSent->created;
-	  $Friend_Name = $database->query("SELECT name FROM {users_field_data} WHERE uid = :uid", array(':uid' => $Friend_ID))->fetchField();
-	  $friendString .= "<li><a href='" . $home_url . "user/" . $Friend_ID ."'>" . $Friend_Name . "</a></li>";
-	}
-    
-	$friendString .= "</td>";
-  */
-  /*
-   * Begin 'Friend' column.  For 'friends' we want users that have each flagged each other with the 'friend' flag.  This means there will be a row in the 'flagging' table where
-   * where each of their IDs appear as 'entity_id' and 'uid'
-  */
-
 	$friendString .= "<td valign='top' class='friendsAccepted'><b>Friends</b></br><ul>";
 
+  //Friends column, each user has 'flagged' the other, so are now friends.
   //Start by pulling in records where current UID = uid and getting the entity_id from the table.  These are people the current user has flagged as a friend.
 	
 	$result = $database->select('flagging', 'f')
@@ -262,7 +145,18 @@ class FriendFlagBlock extends Blockbase {
 	    }
 	  }
   $friendString .= "</td></tr></table>";
-
+  $tag_array = array(
+    'a',
+    'b',
+    'br',
+    'div',
+    'li',
+    'table',
+    'td',
+    'tr',
+    'ul',
+  );
+  $friendString = Xss::filter($friendString, $tag_array);
   return $friendString;
 
   }
